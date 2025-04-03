96f008d8bfcd6e81527e914ddf77a6a2eb0440a4
Jonas_Desktop <jonas@bojanovsky.at>
2025-04-03 17:55:16 +0200

    made simple animation program

--- ising/fortran/animate.py
+++ ising/fortran/animate.py
@@ -1,3 +1,22 @@
 import numpy as np
-import matplotlib.animation as anim
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+
+# Load all frames
+data = [np.loadtxt(f"snapshot{i:03d}.txt") for i in range(100)]  # Assumes files are named 000.npy to 099.npy
+
+# Setup plot
+fig, ax = plt.subplots()
+im = ax.imshow(data[0], cmap='gray', vmin=-1, vmax=1)
+
+def update(frame):
+    im.set_array(data[frame])
+    return [im]
+
+ani = animation.FuncAnimation(fig, update, frames=len(data), interval=50, blit=True)
+
+plt.show()
+
+# Optional: save as mp4
+# ani.save('animation.mp4', fps=20)
