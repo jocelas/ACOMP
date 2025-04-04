b699c25cfb1bede15eaf9c0cd46d873e1c6dd941
jocelas <jonas@bojanovsky.at>
2025-04-04 16:46:23 +0200

    Almost finished with exercise 6 of sheet 2

---
+++ sht2/6_thermodynamic_limit/code.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rho=0.84
+disp=0.3
+t=0.694
+
+i=0
+for n in 5 10 25 50 108 256 500; do
+
+    printf "Running with n = $n ...... \n\n"
+
+    mkdir n$n
+
+    ../lj/gmclj $n $rho $t $disp 1 1 1
+
+    ../lj/mclj > "n$n/equil.dat"
+
+    printf "Equilibrated :)"
+
+    ../lj/zmclj 1 1 10000
+
+    ../lj/mclj | tee "n$n/data.dat"
+
+    ../lj/amclj | tee "n$n/result.txt"
+
+    printf "____________________\n\n\n\n"
+
+    mv *.dat *.pdb traj.xyz n$n
+
+    ((i++))
+
+done
+
+echo "Done"
